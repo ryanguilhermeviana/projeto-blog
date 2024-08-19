a302f4a6371c38c87440edf5d24b2bb450547846
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-08-19 16:30:34 -0300

    PostManager

--- blog/models.py
+++ blog/models.py
@@ -83,11 +83,19 @@ class Page(models.Model):
     def __str__(self) -> str:
         return self.title
     
+class PostManager(models.Manager):
+    def get_published(self):
+        return self\
+            .filter(is_published=True)\
+            .order_by('-pk')
+
+    
 class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
+    objects = PostManager()
     title = models.CharField(max_length=65,)
     slug = models.SlugField(
         unique=True, default="",
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 PER_PAGE = 9
 def index(request):
-    posts = (Post.objects.filter(is_published=True).order_by('-pk'))
+    posts = Post.objects.get_published()
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
