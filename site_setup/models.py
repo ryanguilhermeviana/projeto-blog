84da64bf2a9c1323ccab0e3100dd07e378893b75
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-08-19 11:16:56 -0300

    resize image

--- site_setup/models.py
+++ site_setup/models.py
@@ -32,21 +32,21 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
-    # favicon = models.ImageField(
-    #     upload_to='assets/favicon/%Y/%m/', blank=True, default='',
-    #     validators=[validate_png],
-    # )
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/', blank=True, default='',
+        validators=[validate_png],
+    )
 
-    # def save(self, *args, **kwargs):
-    #     current_favicon_name = str(self.favicon.name)
-    #     super().save(*args, **kwargs)
-    #     favicon_changed = False
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
 
-    #     if self.favicon:
-    #         favicon_changed = current_favicon_name != self.favicon.name
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
 
-    #     if favicon_changed:
-    #         resize_image(self.favicon, 32)
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
 
     def __str__(self):
