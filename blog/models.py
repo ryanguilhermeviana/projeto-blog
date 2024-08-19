e5e5186d540fffc87abb488d3381a8a6ea7114b7
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-08-19 14:48:05 -0300

    summer_note

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from utils.rands import slugify_new
 from utils.image import resize_image
+from django_summernote.models import AbstractAttachment
 
 # Create your models here.
+class PostAttachment(AbstractAttachment):
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900, True, 70)
+
+        return super_save
+    
 class Tag(models.Model):
     class Meta:
         verbose_name = 'Tag'
--- project/urls.py
+++ project/urls.py
@@ -21,8 +21,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
