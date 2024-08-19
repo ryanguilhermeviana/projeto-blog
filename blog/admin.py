1dd34972c25935d21f136f723bb07907cf7c9dd2
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-08-19 16:49:48 -0300

    function link

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from blog.models import Category, Tag, Page, Post
 from django_summernote.admin import SummernoteModelAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 @admin.register(Tag)
@@ -42,7 +44,7 @@ class PageAdmin(SummernoteModelAdmin):
     }
 
 @admin.register(Post)
-class PageAdmin(SummernoteModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
@@ -51,11 +53,22 @@ class PageAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', 'link',)
     prepopulated_fields = {
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{url_do_post}">Ver post</a>'
+        )
+
+        return safe_link
 
     def save_model(self, request, obj, form, change):
         if change:
