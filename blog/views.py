7c1e448941f92ee77e79fcb7bc0436995cc2e3dc
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-08-19 14:57:55 -0300

    corrigindo pagination e views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
+PER_PAGE = 9
 def index(request):
-    return render(request, 'blog/pages/index.html')
+    posts = (Post.objects.filter(is_published=True).order_by('-pk'))
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
 
 def page(request):
     return render(request, 'blog/pages/page.html')
